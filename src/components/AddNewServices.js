98763316477dede8d07f192ec4cf858b03be8fea
Matis121 <mateusz6246@gmail.com>
2023-06-17 11:41:01 +0200

    implementation of zustanad

--- src/components/AddNewServices.js
+++ src/components/AddNewServices.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import { useServiceStore } from "../stores/store";
 
 const AddNewServices = (props) => {
+  const addService = useServiceStore((state) => state.addService);
+  const serviceStore = useServiceStore((state) => state.services);
+
+  const serviceNameRef = useRef();
+  const servicePriceRef = useRef();
+  const serviceTimeRef = useRef();
+  const amountOfServices = serviceStore.length;
+
+  const addNewService = () => {
+    const newService = {
+      id: amountOfServices, 
+      serviceName: serviceNameRef.current.value, 
+      servicePrice: servicePriceRef.current.value, 
+      serviceTime: serviceTimeRef.current.value
+    };
+    addService(newService);
+    console.log(newService);
+  }
+  
   return (
-    <div className={`${props.showAddSericeBox === true ? "flex" : "hidden" } fixed flex-col gap-6 items-center justify-center bg-slate-200 p-24 rounded-xl`}>
-        <button className='absolute top-3 right-5 bg-slate-300 p-2 rounded-md hover:cursor-pointer' onClick={() => props.setshowAddSericeBox(false)}>Close</button>
+    <div className={`${props.showAddServiceBox === true ? "flex" : "hidden" } fixed flex-col gap-6 items-center justify-center bg-slate-200 p-24 rounded-xl`}>
+        <button className='absolute top-3 right-5 bg-slate-300 p-2 rounded-md hover:cursor-pointer' onClick={() => props.setShowAddServiceBox(false)}>Close</button>
         <p className='text-lg'>Dodaj usługę</p>
-        <input type="text" onChange={(e) => props.setName(e.target.value)} value={props.name} name='firstName' placeholder='nazwa' className='border rounded-md p-2 mr-2' />
-        <input type="text" onChange={(e) => props.setServicePrice(e.target.value)} value={props.servicePrice} name='surName' placeholder='cena' className='border rounded-md p-2 mr-2'/>
-        <input type="number" onChange={(e) => props.setServiceTime(e.target.value)} value={props.serviceTime} name='phone' placeholder='czas' className='border rounded-md p-2 mr-2'/>
-        <button onClick={props.addNewService} className='border rounded-md px-6 py-2 hover:cursor-pointer bg-slate-400'>Dodaj</button>
+        <input type="text" ref={serviceNameRef}  placeholder='nazwa' className='border rounded-md p-2 mr-2' />
+        <input type="text" ref={servicePriceRef}  placeholder='cena' className='border rounded-md p-2 mr-2'/>
+        <input type="number" ref={serviceTimeRef}  placeholder='czas' className='border rounded-md p-2 mr-2'/>
+        <button onClick={addNewService} className='border rounded-md px-6 py-2 hover:cursor-pointer bg-slate-400'>Dodaj</button>
     </div>
   )
 }
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,15 +1,72 @@
+import { useEffect } from "react";
 import { create } from "zustand";
-import { devtools } from "zustand/middleware"
+import { devtools, persist } from "zustand/middleware"
 
-let useServiceStore = (set) => ({
-    services: [],
-    addService: (service) =>
-        set((state) => ({services: [...state.services, service]})),
-});
+// SERVICE STORE
+
+// const ServiceStore = persist((set) => ({
+//     services: [],
+//     addService: (service) =>
+//         set((state) => ({services: [...state.services, service]})),
+//     removeService: (id) => 
+//         set((state) => ({ services: state.services.filter((service) => service.id !== id) })),
+        
+// }));
+
+// export const useServiceStore = create(
+//     devtools(ServiceStore)
+// );
+
+const ServiceStore = persist(
+    devtools((set) => ({
+      services: [],
+      addService: (service) =>
+        set((state) => ({ services: [...state.services, service] })),
+      removeService: (id) =>
+        set((state) => ({
+          services: state.services.filter((service) => service.id !== id),
+        })),
+    })),
+    {
+      name: "serviceStore", // Specify a unique name for the store
+      getStorage: () => localStorage, // Use localStorage as the storage provider
+    }
+  );
+  
+  export const useServiceStore = create(ServiceStore);
 
 
-let useClientStore = (set) => ({
-    clients: [],
-    addClient: (client) =>
-        set((state) => ({clients: [...state.clients, client]})),
-});
+
+// CLIENT STORE
+
+// const ClientStore = (set) => ({
+//     clients: [],
+//     addClient: (client) =>
+//         set((state) => ({clients: [...state.clients, client]})),
+//     removeClient: (id) => 
+//         set((state) => ({ clients: state.clients.filter((client) => client.id !== id) })),
+// });
+
+
+// export const useClientStore = create(
+//     devtools(ClientStore)
+// );
+
+const ClientStore = persist(
+    devtools((set) => ({
+      clients: [],
+      addClient: (client) =>
+        set((state) => ({ clients: [...state.clients, client] })),
+      removeClient: (id) =>
+        set((state) => ({
+          clients: state.clients.filter((client) => client.id !== id),
+        })),
+    })),
+    {
+      name: "clientStore", // Specify a unique name for the store
+      getStorage: () => localStorage, // Use localStorage as the storage provider
+    }
+  );
+  
+  export const useClientStore = create(ClientStore);
+
