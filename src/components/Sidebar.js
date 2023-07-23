7b7eb477bfb59968d81709523b121fd78dcd6edf
Matis121 <mateusz6246@gmail.com>
2023-07-23 23:24:25 +0200

    implement calnedar into app

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,16 +10,16 @@ const Sidebar = () => {
                 <Link to="/" className='hover:cursor-pointer'>Home</Link>
             </div>
             <div className='py-8 px-4 flex items-center justify-center text-white'>
-                <Link to="/calendar" className='hover:cursor-pointer'>Kalendarz</Link>
+                <Link to="/calendar" className='hover:cursor-pointer'>Calendar</Link>
             </div>
             <div className='py-8 px-4 flex items-center justify-center text-white'>
-                <Link to="/clients" className='hover:cursor-pointer'>Klienci</Link>
+                <Link to="/clients" className='hover:cursor-pointer'>Clients</Link>
             </div>
             <div className='py-8 px-4 flex items-center justify-center text-white'>
-                <Link to="/services" className='hover:cursor-pointer'>Usługi</Link>
+                <Link to="/services" className='hover:cursor-pointer'>Services</Link>
             </div>
             <div className='py-8 px-4 flex items-center justify-center text-white'>
-                <a className='hover:cursor-pointer'>Ustawienia</a>
+                <a className='hover:cursor-pointer'>Settings</a>
             </div>
         </section>
     </>
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,11 +1,93 @@
 import React from 'react';
+import { useState } from 'react';
 import Sidebar from '../components/Sidebar';
+import AddNewEventToCalendar from '../components/AddNewEventToCalendar';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <strong>{eventInfo.timeText}</strong>
+      <p>{eventInfo.event.title}</p>
+      {eventInfo.event.extendedProps.description && (
+        <p>{eventInfo.event.extendedProps.description}</p>
+      )}
+    </>
+  );
+}
 
 const Calendar = () => {
 
+  const events = [
+    {
+      title: "Our first client",
+      start: "2023-07-22T08:00:00",
+      end: "2023-07-22T10:00:00",
+      description: "This is the description for Event 1.",
+    },
+    {
+      title: "Our first client",
+      start: "2023-07-22T11:00:00",
+      end: "2023-07-22T12:00:00",
+    },
+  ];
+
+  const [showAddNewEvent, setShowAddNewEvent] = useState(false);
+  const [allEvents, setAllEvents] = useState(events);
+  const [newEvent, setNewEvent] = useState({title: "", start: "", end: "", description: ""});
+
+  const handleAddEvents = () => {
+    setAllEvents([...allEvents, newEvent])
+  }
+
+  const eventContent = (arg) => {
+    return (
+      <div className="overflow-hidden h-full">
+        {arg.timeText}
+        <br></br>
+        <strong>{arg.event.title}</strong>
+        <br></br>
+        <i>{arg.event.extendedProps.description}</i>
+      </div>
+    )
+  }
+
   return (
     <div className='flex'>
-      <Sidebar />
+      <Sidebar/>
+      <div class="w-full"> 
+      <AddNewEventToCalendar setShowAddNewEvent={setShowAddNewEvent} showAddNewEvent={showAddNewEvent} setNewEvent={setNewEvent} newEvent={newEvent} handleAddEvents={handleAddEvents}/>   
+        <FullCalendar
+          plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+          initialView="timeGridWeek"
+          allDaySlot={false}
+          headerToolbar={{
+            start: "today prev,next",
+            center: "title",
+            end: "dayGridMonth,timeGridWeek,timeGridDay"
+          }}
+          height={"100vh"}
+          events={allEvents}
+          selectable={true}
+          eventContent={eventContent}
+          select ={function(start, end){
+            console.log("siema");
+            setNewEvent({
+              ...newEvent,
+              start: start.startStr,
+              end: start.endStr,
+            });
+            setShowAddNewEvent(true);
+          }}
+          editable={true}
+          eventDrop={function(event){
+            console.log(event);
+          }}
+        />
+      </div>
     </div>
   )
 }
