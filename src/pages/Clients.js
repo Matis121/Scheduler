f2397054c720816664b694881e2a8cb3c61f66d6
Matis121 <mateusz6246@gmail.com>
2023-08-15 00:24:09 +0200

    remove all errors from console

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -26,45 +26,45 @@ const Clients = () => {
               {clients.length > 0 ? <button className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 absolute right-20' onClick={() => setShowAddClientBox(!showAddClientBox)}>Add new client</button> : null}
             </div>
             {clients.length === 0 ? <span className='flex flex-col gap-8 self-center mt-48 p-12 rounded-xl'>You dont have any clients yet<button className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800' onClick={() => setShowAddClientBox(!showAddClientBox)}>Add new client</button></span> :             
-            <div class="relative overflow-x-auto shadow-md w-full">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="relative overflow-x-auto shadow-md w-full">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 LP
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Firstname
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Surname 
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Phone number
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                <span class="sr-only">Edit</span>
+                            <th scope="col" className="px-6 py-3">
+                                <span className="sr-only">Edit</span>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         {clients.map((client, idx) => 
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td class="px-6 py-4">
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={idx}>
+                            <td className="px-6 py-4">
                             {idx + 1}
                             </td>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {client.firstName}
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                             {client.surName}
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                             {client.phoneNumber}
                             </td>
-                            <td class="px-6 py-4 text-right">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-4" onClick={() => handleDeleteClient(client.id)}>Edit</a>
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onClick={() => handleDeleteClient(client.id)}>Delete</a>
+                            <td className="px-6 py-4 text-right">
+                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-4" onClick={() => handleDeleteClient(client.id)}>Edit</a>
+                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" onClick={() => handleDeleteClient(client.id)}>Delete</a>
                             </td>
                         </tr>
                         )}
